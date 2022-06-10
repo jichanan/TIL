ab60b006bb4027aa3b99c81ef7ed6cc31a5a715c
Jichan An <dkswlcks@gmail.com>
2022-06-10 17:46:17 +0900

    conditional statement

--- Python/Python studying alone/handcoding/ampm.py
+++ Python/Python studying alone/handcoding/ampm.py
@@ -6,3 +6,4 @@ if now.hour >= 12:
     print("지금은 {}시로 오후입니다.".format(now.hour))
 if now.hour < 12:
     print("지금은 {}시로 오전입니다.".format(now.hour))
+
--- Python/Python studying alone/handcoding/oddeven.py
+++ Python/Python studying alone/handcoding/oddeven.py
@@ -4,3 +4,4 @@ if number % 2 == 0:
     print("짝수입니다.")
 if number % 2 == 1:
     print("홀수입니다.")
+    
