4fface998621707978e0e1ddaaf8e1efcc39104a
Jichan An <dkswlcks@gmail.com>
2022-06-17 12:01:22 +0900

    Python

---
+++ Python/Python studying alone/handcoding/flatting.py
@@ -0,0 +1,19 @@
+character = {
+    "name":"기사",
+    "level":12,
+    "items":{
+        "sword":"불꽃의 검",
+        "armor":"풀플레이트"
+    },
+    "skill": ["베기", "세게 베기", "아주 세게 베기"]
+}
+
+for key in character:
+    if type(character[key]) is dict:
+        for k in character[key]:
+            print("{} : {}".format(k,character[key][k])) 
+    elif type(character[key]) is list:
+        for a in character[key]:
+            print("{} : {}".format(key,a)) 
+    else:
+        print("{} : {}".format(key,character[key]))
